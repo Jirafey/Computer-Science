9912ea5f92e6c7a342bfb1ca63ac874f37e9a133
Jirafey <97115044+Jirafey@users.noreply.github.com>
2022-11-21 15:41:01 +0100

    commit finished version

--- C++/projects/Programowanie-LAB/zut/lab5/lab5-old.cpp
+++ C++/projects/Programowanie-LAB/zut/lab5/lab5-old.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cmath>
-#include "dodatnie_liczby_refactored.cpp"
 using namespace std;
+
 // can make a program to put all of the text into arrays and put inside the functions and template
 
 // PĘTLE
 
 
+
 int num1, num2, num3;
-int * addresses[3] = {&num1, &num2, &num3};
+int * addresses[3] = {&num1,&num2, &num3};
 
 int * dodatnie_liczby(int var_num) {
+    int arr[3+1];
     if (var_num == 3) {
         while (true) {
             cout << "Enter 3 positive integers" << endl;
@@ -19,23 +21,34 @@ int * dodatnie_liczby(int var_num) {
                 continue;
             }
             else {
-                cin >> num2;
+                arr[0] = num1;
+                cin >> num1;
             }    
-            if (num2 < 0) { 
+            if (num1 < 0) { 
                 continue;
             }
             else {
-                cin >> num3;
+                arr[1] = num1;
+                cin >> num1;
             }
-            if (num3 < 0) {
+            if (num1 < 0) {
                 continue;
+                
             }
             else {
+                arr[2] = num1;
                 break;
             }
         }
         printf("\n");
-        return  addresses[3];
+        cout << "array2\n" << arr[0] << endl;
+        cout << "array3\n" << arr[1] << endl;
+        cout << "array4\n" << arr[2] << endl;
+        * addresses =  arr; 
+        cout << addresses[0]<<endl;
+        cout << addresses[1]<<endl;
+        cout << addresses[2]<<endl;
+        return  addresses[3+1];
     }
     
     if (var_num == 2) {
@@ -204,61 +217,14 @@ void zad8(){
 // 9. Napisz program, który wczytuje ze standardowego wejścia dodatnie liczby całkowite n i m, i wypisuje na
 // standardowym wyjściu największy wspólny dzielnik tych liczb.
 void zad9(){
-
-    int n, m;
-    int wynik = 0;
-
-    cout << "Wprowadz liczbe n: ";
-    cin >> n;
-    while (n <= 0) {
-        cout << "Liczba musi byc dodatnia" << endl;
-        cout << "Wprowadz liczbe n: ";
-        cin >> n;
-    }
-
-    cout << "Wprowadz liczbe m: ";
-    cin >> m;
-    while (m <= 0) {
-        cout << "Liczba musi byc dodatnia" << endl;
-        cout << "Wprowadz liczbe m: ";
-        cin >> m;
-    }
-
-    for (int i = 1; i <= m && i <= n; i++) {
-        if (n % i == 0) {
-            if (m % i == 0) {
-                wynik = i;
-            }
-        }
-    }
-    cout << wynik << endl;
+    dodatnie_liczby(2);
 }
 
 // 10. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę n i wypisuje na standardowym
 // wyjściu wartość ⌊√n⌋ (wartość √n zaokrągloną w dół do najbliższej wartości całkowitoliczbowej). Program
 // napisz bez użycia funkcji z biblioteki matematycznej.
 void zad10(){
-
-    float n2, est, prev_est = 0;
-    float delta = 0.01;
-
-    cout << "Wprowadz liczbe n: ";
-    cin >> n2;
-    while (n2 <= 0) {
-        cout << "Liczba musi byc dodatnia" << endl;
-        cout << "Wprowadz liczbe n: ";
-        cin >> n2;
-    }
-
-    est = n2 / 2.0;
-    prev_est = est + 2 * delta;
-
-    while (abs(prev_est - est) > delta) {
-        prev_est = est;
-        est = (est + n2 / est) / 2.0;
-    }
-
-    cout << trunc(est) << endl;
+    dodatnie_liczby(1);
 
 }
 
@@ -270,69 +236,17 @@ void zad10(){
 // c. wypisuje na standardowe wyjście największą nieujemną liczbę całkowitą x taką, że 5∗x2+a∗x+b ≤ c.
 // Zakładamy, że taka nieujemna całkowita liczba x istnieje.
 void zad11(){
-    int a, b, c, d;
-    int x = 1;
-    
-    cout << "Wprowadz a: ";
+    int a,b,c,d;
     cin >> a;
-    cout << "Wprowadz b: ";
     cin >> b;
-    cout << "Wprowadz c: ";
     cin >> c;
-    cout << "Wprowadz d: ";
     cin >> d;
-
-    while (!((abs(a) * x * x + b * x + c) > d)) {
-        x++;
-    }
-
-    cout << x << endl;
-
-    x = 1;
-
-    while (5 * x * x + a * x + b < c) {
-        x++;
-    }
-
-    cout << x-1 << endl;
-
-    x = 1;
-
-    while (5 * x * x + a * x + b <= c) {
-        x++;
-    }
-
-    cout << x - 1 << endl;
 }
 
 // 12. Napisz program, który wczytuje ze standardowego wejścia dodatnią liczbę n i wypisuje na standardowym
 // wyjściu sumę wszystkich liczb mniejszych od n, względnie pierwszych z n.
 void zad12(){
-    int i, j, n3;
-    int suma = 0;
-    int nwd = 0;
-    i = 2;
-
-    cout << "Wprowadz n: ";
-    cin >> n3;
-
-    while (i < n3) {
-        nwd = 0;
-        j = 1;
-        while (j < i) {
-            if (i % j == 0) {
-                nwd = j;
-            }
-            j++;
-        }
-        if (nwd == 1) {
-            suma = suma + j;
-            cout << j << endl;
-        }
-        i++;
-    }
-    cout << "Suma liczb wzglednie pierwszych: " << suma << endl;
-    
+    dodatnie_liczby(1);
 }
 
 // 13. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n i wypisuje na
@@ -352,20 +266,10 @@ void zad13(){
 // mniejszych od n.
 void zad14(){
     int n;
+    cin >> n;
+    // for () {
 
-        cout << "Wprowadz liczbe n: ";
-        cin >> n;
-
-        for (int a = 1; a < n; a++) {
-            for (int b = 1; b < n; b++) {
-                for (int c = 1; c < n; c++) {
-                    if (a * a + b * b == c * c && a < b) {
-                        cout << a << " " << b << " " << c << endl;
-                    }
-                }
-            }
-        }
-
+    // }
 }
 int main(){
     // // Zad 1
@@ -377,25 +281,25 @@ int main(){
     // printf("\nZad.2\n");
     // zad2();
 
-    // // Zad 3
-    // printf("Zad.3\n");
-    // zad3();
+    // Zad 3
+    printf("Zad.3\n");
+    zad3();
     
-    // // Zad 4
-    // printf("Zad.4\n");
-    // zad4();
+    // Zad 4
+    printf("Zad.4\n");
+    zad4();
     
-    // // Zad 5
-    // printf("Zad.5\n");
-    // zad5();
+    // Zad 5
+    printf("Zad.5\n");
+    zad5();
     
-    // // Zad 6
-    // printf("Zad.6\n");
-    // zad6();
+    // Zad 6
+    printf("Zad.6\n");
+    zad6();
     
-    // // Zad 7
-    // printf("Zad.7\n");
-    // zad7();
+    // Zad 7
+    printf("Zad.7\n");
+    zad7();
     
     // Zad 8
     printf("Zad.8\n");
--- C++/projects/Programowanie-LAB/zut/lab5/usual_lab.cpp
+++ C++/projects/Programowanie-LAB/zut/lab5/usual_lab.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
 using namespace std;
-// PĘTLE
-// 1. Napisz program wczytujący ze standardowego wejścia dwie dodatnie liczby całkowite n i m, i wypisujący w
-// kolejnych wierszach na standardowym wyjściu wszystkie dodatnie wielokrotności n mniejsze od m.
+
 void zad1(){
     
 }
@@ -46,6 +44,57 @@ void zad13(){
 void zad14(){
     
 }
+void zad15(){
+    
+}
+void zad15(){
+    
+}
+void zad16(){
+    
+}
+void zad17(){
+    
+}
+void zad18(){
+    
+}
+void zad19(){
+    
+}
+void zad20(){
+    
+}
+void zad21(){
+    
+}
+void zad22(){
+    
+}
+void zad23(){
+    
+}
+void zad24(){
+    
+}
+void zad25(){
+    
+}
+void zad26(){
+    
+}
+void zad27(){
+    
+}
+void zad28(){
+    
+}
+void zad29(){
+    
+}
+void zad30(){
+    
+}
 int main(){
     zad1();
     
@@ -74,40 +123,36 @@ int main(){
     zad13();
     
     zad14();
-}
 
-// 2. Napisz program wczytujący ze standardowego wejścia dwie dodatnie liczby całkowite n i m, i wypisujący na
-// standardowym wyjściu m pierwszych wielokrotności liczby n.
-// 3. Napisz program wczytujący ze standardowego wejścia trzy dodatnie liczby całkowite n, m i k, i wypisujący w
-// kolejnych wierszach wszystkie wielokrotności n większe od m i mniejsze od k.
-// 4. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n i wypisuje na
-// standardowym wyjściu liczbę n!.
-// 5. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n i wypisuje na
-// standardowym wyjściu sumę kwadratów liczb od 0 do n, czyli wartość 02 + 12 + 32 + ... + n2.
-// 6. Napisz program, który wczytuje ze standardowego wejścia liczbę całkowitą n (n > 2) i wypisuje na
-// standardowym wyjściu iloczyn liczb parzystych z zakresu od 2 do n (czyli 2 ∗ 4 ∗ . . . ∗ n dla n parzystych i 2 ∗
-// 4 ∗ . . . ∗ (n − 1) w przeciwnym wypadku).
-// 7. Napisz program, który wczytuje ze standardowego wejścia dwie liczby całkowite n i m (zakładamy, że n < m)
-// i wypisuje na standardowym wyjściu liczbę n ∗ . . . ∗ m.
-// 8. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n i wypisuje na
-// standardowym wyjściu element ciągu Fibonacciego o indeksie n.
-// 9. Napisz program, który wczytuje ze standardowego wejścia dodatnie liczby całkowite n i m, i wypisuje na
-// standardowym wyjściu największy wspólny dzielnik tych liczb.
-// 10. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę n i wypisuje na standardowym
-// wyjściu wartość ⌊√n⌋ (wartość √n zaokrągloną w dół do najbliższej wartości całkowitoliczbowej). Program
-// napisz bez użycia funkcji z biblioteki matematycznej.
-// 11. Napisz program, który wczytuje ze standardowego wejścia liczby a, b, c, d i:
-// a. wypisuje na standardowe wyjście najmniejszą nieujemną liczbę całkowitą x taką, że |a| ∗ x2 + b ∗ x + c >
-// d.
-// b. wypisuje na standardowe wyjście największą nieujemną liczbę całkowitą x taką, że 5∗x2+a∗x+b < c.
-// Zakładamy, że taka nieujemna całkowita liczba x istnieje.
-// c. wypisuje na standardowe wyjście największą nieujemną liczbę całkowitą x taką, że 5∗x2+a∗x+b ≤ c.
-// Zakładamy, że taka nieujemna całkowita liczba x istnieje.
-// 12. Napisz program, który wczytuje ze standardowego wejścia dodatnią liczbę n i wypisuje na standardowym
-// wyjściu sumę wszystkich liczb mniejszych od n, względnie pierwszych z n.
-// 13. Napisz program, który wczytuje ze standardowego wejścia nieujemną liczbę całkowitą n i wypisuje na
-// standardowym wyjściu wartość 0! + 1! + . . . + n!.
-// 14. Napisz program, który wczytuje ze standardowego wejścia liczbę n i wypisuje na standardowym wyjściu
-// wszystkie trojki pitagorejskie (tj. trojki liczb całkowitych a, b, c takich, że a2 +b2 = c2), składające się z liczb
-// mniejszych od n.
- 
+    zad15();
+    
+    zad16();
+
+    zad17();
+    
+    zad18();
+    
+    zad19();
+    
+    zad20();
+    
+    zad21();
+    
+    zad22();
+    
+    zad23();
+    
+    zad24();
+    
+    zad25();
+    
+    zad26();
+    
+    zad27();
+    
+    zad28();
+    
+    zad29();
+    
+    zad30();
+}
